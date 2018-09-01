23fa5fceed9970cfdf0f5c473c5f3bccd75184ff
ameypol <amey.pol86@gmail.com>
2018-09-01 22:35:50 +0530

    spring boot starter

---
+++ springapp/src/main/java/springapp/topic/Topic.java
@@ -0,0 +1,37 @@
+package springapp.topic;
+
+public class Topic {
+
+	
+	private String id;
+	private String name;
+	private String desccription;
+	
+	public Topic( String id, String name, String desccription) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.desccription = desccription;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDesccription() {
+		return desccription;
+	}
+	public void setDesccription(String desccription) {
+		this.desccription = desccription;
+	}
+	
+	
+	
+}
---
+++ springapp/src/main/java/springapp/topic/TopicController.java
@@ -0,0 +1,35 @@
+package springapp.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicsservice;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getallTopic(){
+		return topicsservice.getlist();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopica(@PathVariable String id){
+		
+		return topicsservice.getTopic(id);
+		
+	}
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicsservice.addTopic(topic); 
+		
+	}
+	
+}
