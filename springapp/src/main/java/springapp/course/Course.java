4d5bb12a426973f51d5ecf14776962a116f1cc2a
ameypol <amey.pol86@gmail.com>
2018-09-02 16:01:15 +0530

    Complete spring boot CRUD application

--- springapp/src/main/java/springapp/course/Course.java
+++ springapp/src/main/java/springapp/course/Course.java
@@ -1,24 +1,37 @@
-package springapp.topic;
+package springapp.course;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import springapp.topic.Topic;
 
 
 @Entity
-public class Topic {
+public class Course {
 
 	@Id
 	private String id;
 	private String name;
 	private String desccription;
 	
-	public Topic( String id, String name, String desccription) {
+	@ManyToOne
+	private Topic topic;
+	
+	public Topic getTopic() {
+		return topic;
+	}
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+	public Course( String id, String name, String desccription, String topicId) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.desccription = desccription;
+		this.topic= new Topic(topicId,"","");
 	}
-	public Topic() {
+	public Course() {
 		// super();
 		// TODO Auto-generated constructor stub
 	}
